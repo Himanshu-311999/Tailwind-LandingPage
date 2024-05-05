3a023a8a72047e364465047b65fb196b57f6b2ec
Himanshu Mukhedkar <himanshumukhedkar@gmail.com>
2024-05-05 15:47:31 +0530

    Added dark and light mode support

--- TailwindProject/src/App.jsx
+++ TailwindProject/src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  useEffect(() => {
+    if(theme=== 'light'){
+      document.documentElement.classList.add("dark")
+    } 
+    else{
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme])
+  
 
   return (
-    <div className="min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:justify-between">
-      <Header />
+    <div className="dark:bg-black dark:text-white min-h-screen px-6 py-3 bg-amber-50 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-16 lg:justify-between">
+      <Header setTheme={setTheme} theme={theme} />
       <Body />
       <Footer />
     </div>
--- TailwindProject/src/components/Header.jsx
+++ TailwindProject/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { FaBars } from 'react-icons/fa'
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
 
-const Header = () => {
+const Header = ({ setTheme, theme }) => {
+
+    const handleThemeSwitch = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
+
     return (
         <div className='flex justify-between'>
 
             <div className='flex items-center justify-center gap-2'>
-                <img src="./assets/Logo.svg" alt="logo" />
+                {theme==="light" ? <div className='font-bold px-2 lg:hidden'>{"HOSTER"}</div>:<img src="./assets/Logo.svg" alt="logo" />}
                 <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl font-lato'>Hoster is hiring</button>
             </div>
             <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6'>
@@ -18,9 +25,10 @@ const Header = () => {
                 <a href="#" className='text-gray-400 font-lato'>Sign In</a>
                 <button className='rounded-md px-4 py-3 bg-blue-400  hover:bg-blue-600 text-white'>Join Waitlist</button>
             </div>
-            <div className='lg:hidden'>
+            <div className=' py-1 lg:hidden'>
                 <FaBars />
             </div>
+            <button className='text-2xl md:text-3xl lg:text-4xl' onClick={handleThemeSwitch}>{theme === "dark" ? <MdDarkMode /> : <MdOutlineDarkMode />}</button>
         </div>
     )
 }
